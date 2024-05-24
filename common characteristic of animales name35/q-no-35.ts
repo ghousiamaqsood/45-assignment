da683dba01e101caf48fd54179a94bfa43f31936
ghousia maqsoog <164852766+ghousiamaqsood@users.noreply.github.com>
2024-05-24 23:20:51 +0500

    first commit

---
+++ common characteristic of animales name35/q-no-35.ts
@@ -0,0 +1,12 @@
+// Creating a Array
+
+let petAnimals = [ "Cat", "Dog","Rabbit"]
+
+// Using for-loop
+
+for (let onepet of petAnimals)
+    {
+        console.log(`${onepet} would you make agreat pet`)
+       // print a message outside of for-loop
+       console.log("Any of these animals would make a agreat pet!S")
+    }
